98fe1e5ec79f63f6a2dfb9aefeca93b0fa861294
pd438 <pd438@njit.edu>
2023-11-27 17:40:40 -0500

    Milestone 2 update pt6

--- public_html/project/home.php
+++ public_html/project/home.php
@@ -9,13 +9,12 @@ if(is_logged_in()){
 else{
     flash("You're not logged in.  Log in Dude");
 }
-<div class="container-fluid">
-    <div class="h-50 p-5 text-bg-dark rounded-3">
-        <h1>Welcome to Random Cookbook!</h1>
-        <p>Thank you for your interest in our mission. We are a organization with a goal to find you the best recipes while saving your wallet. Please give this a try and you will be satisfied.  I guarantee it. </p>
-        <p class="text-center"><a class="btn btn-primary btn-lg" href="<?php get_url("browse.php", true); ?>" role="button">Let us Begin!!!!</a></p>
-    </div>
-</div>
-<?php
-require(__DIR__."/../../partials/footer.php");
+/*if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
+ echo "Welcome, " . $_SESSION["user"]["email"]; 
+}
+else{
+  echo "You're not logged in";
+}*/
+
+require(__DIR__."/../../partials/flash.php");
 ?>
