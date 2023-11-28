b06bf42cc204831d4e3e03c50549128e071513fe
pd438 <pd438@njit.edu>
2023-11-27 20:54:28 -0500

    Tables Update

--- public_html/project/sql/recipes.sql
+++ public_html/project/sql/recipes.sql
@@ -6,9 +6,3 @@ CREATE TABLE Recipes (
     CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     ModifiedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-
-INSERT INTO Recipes (RecipeID, Title, ImageURL, ImageType)
-VALUES
-    (1079930, 'Crock Pot Pasta Bolognese Sauce', 'https://spoonacular.com/recipeImages/1079930-312x231.jpg', 'jpg'),
-    (474589, 'Crockpot Turkey Bolognese Pasta Sauce', 'https://spoonacular.com/recipeImages/474589-312x231.jpg', 'jpg'),
-    (543261, 'Crock Pot Meaty Pasta Sauce', 'https://spoonacular.com/recipeImages/543261-312x231.jpg', 'jpg');
