6d84117d4456d9c3e653ddc0a1065ec0b4514e45
pd438 <pd438@njit.edu>
2023-12-13 11:38:56 -0500

    Milestone3 Final Changes

--- public_html/project/search_form.php
+++ public_html/project/search_form.php
@@ -3,7 +3,7 @@ require_once(__DIR__ . "/../../partials/nav.php");
 
 // Initialize the variable
 $resultCount = 0;
-
+$db = getDB(); 
 if ($_SERVER["REQUEST_METHOD"] === "GET") {
     // Check if the form is submitted
     if (!empty($_GET["search"])) {  // Check if the search parameter is not empty
@@ -40,71 +40,108 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
                 return $response;
             }
         }
+        function insertLikedRecipe($userId, $recipeId)
+        {
+            $db = getDB();
+            
+            // Prepare the SQL statement
+            $stmt = $db->prepare("INSERT INTO liked_recipes (user_id, RecipeID) VALUES (:user_id, :recipe_id)");
+            
+            // Bind parameters
+            $stmt->bindParam(':user_id', $userId);
+            $stmt->bindParam(':recipe_id', $recipeId);
+            
+            // Execute the statement
+            $stmt->execute();
+        }
+        
+// // Make Spoonacular API request
+$spoonacularResponse = make_request($spoonacularUrl);
 
-        // Make Spoonacular API request
-        $spoonacularResponse = make_request($spoonacularUrl);
+// Process Spoonacular API response
+if ($spoonacularResponse) {
+    $spoonacularData = json_decode($spoonacularResponse, true);
 
-        // Process Spoonacular API response
-        if ($spoonacularResponse) {
-            $spoonacularData = json_decode($spoonacularResponse, true);
+    // Check for JSON decoding errors
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        echo "Error decoding Spoonacular data. JSON Error: " . json_last_error_msg();
+        // Handle the error as needed
+    } else {
+        // Count the number of results
+        $resultCount = isset($spoonacularData['results']) ? count($spoonacularData['results']) : 0;
 
-            // Check for JSON decoding errors
-            if (json_last_error() !== JSON_ERROR_NONE) {
-                echo "Error decoding Spoonacular data. JSON Error: " . json_last_error_msg();
-                // Handle the error as needed
-            } else {
-                // Count the number of results
-                $resultCount = isset($spoonacularData['results']) ? count($spoonacularData['results']) : 0;
-
-                // Display Spoonacular results in your desired format
-                if ($resultCount > 0) {
-                    foreach ($spoonacularData['results'] as $recipe) {
-                        // Display recipe information as needed
-                        echo "Recipe Name: " . $recipe['title'] . "<br>";
-                        echo "Recipe ID: " . $recipe['id'] . "<br>";
-                        if (isset($recipe['sourceUrl'])) {
-                            echo "<a href='" . $recipe['sourceUrl'] . "' target='_blank'>View Recipe</a>";
-                        } else {
-                            echo "<a href='https://spoonacular.com/recipes/{$recipe['id']}' target='_blank'>View Recipe</a>";
-                        }
-
-                        // Check if the recipe is liked
-                        $recipeId = $recipe['id'];
-                        $likedRecipes = isset($_SESSION['liked_recipes']) ? $_SESSION['liked_recipes'] : [];
-
-                        if (in_array($recipeId, $likedRecipes)) {
-                            echo "<p style='color: green;'>You liked this recipe!</p>";
-                        } else {
-                            // Add the "like" button and the hidden field for recipe ID
-                            echo "<form method='POST' action='liked_recipes.php'>";
-                            echo "<input type='hidden' name='recipe_id' value='" . $recipeId . "'>";
-                            echo "<button type='submit' name='like'>Like This Recipe</button>";
-                            echo "</form>";
-                        }
-
-                        echo "<hr>";
-                    }
+        // Display Spoonacular results in your desired format
+        if ($resultCount > 0) {
+            foreach ($spoonacularData['results'] as $recipe) {
+                // Display recipe information as needed
+                echo "Recipe Name: " . $recipe['title'] . "<br>";
+                echo "Recipe ID: " . $recipe['id'] . "<br>";
+                if (isset($recipe['sourceUrl'])) {
+                    echo "<a href='" . $recipe['sourceUrl'] . "' target='_blank'>View Recipe</a>";
                 } else {
-                    echo "No recipes found.";
+                    echo "<a href='https://spoonacular.com/recipes/{$recipe['id']}' target='_blank'>View Recipe</a>";
                 }
+
+                $recipeId = $recipe['id'];
+                $likedRecipes = isset($_SESSION['liked_recipes']) ? $_SESSION['liked_recipes'] : [];
+        
+                if (in_array($recipeId, $likedRecipes)) {
+                    echo "<p style='color: green;'>You liked this recipe!</p>";
+                } else {
+                    // Add the "like" button and the hidden field for recipe ID
+                    echo "<form method='POST' action='search_form.php'>";
+                    echo "<input type='hidden' name='recipe_id' value='" . $recipeId . "'>";
+                    echo "<button type='submit' name='like'>Like This Recipe</button>";
+                    echo "</form>";
+        
+                    // Add a check for the form submission and insert the like into the database
+                    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['like'])) {
+                        // Get the recipe ID from the form submission
+                        $likedRecipeId = $_POST['recipe_id'];
+        
+                        // Insert the like into the liked_recipes table
+                        $userId = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
+                        if ($userId) {
+                            insertLikedRecipe($userId, $likedRecipeId);
+        
+                            // Show the notification when the recipe is liked
+                            echo "<script>showNotification('Recipe Liked!');</script>";
+                        }
+        
+                        // Optionally, you can update the session variable to track liked recipes
+                        $_SESSION['liked_recipes'][] = $likedRecipeId;
+                    }
+                }
+
+                echo "<hr>";
             }
         } else {
-            echo "Error retrieving Spoonacular data. Response: " . $spoonacularResponse;
+            echo "No recipes found.";
         }
     }
+} else {
+    echo "Error retrieving Spoonacular data. Response: " . $spoonacularResponse;
 }
 
-require_once(__DIR__ . "/../../partials/footer.php");
+
+    }
+}require_once(__DIR__ . "/../../partials/footer.php");
 ?>
 
 <!-- Add the HTML form for the search button -->
 <form method="GET">
+<script>
+    function showNotification(message) {
+        alert(message);  // You can replace this with a more sophisticated notification library
+    }
+</script>
     <input type="text" name="search" placeholder="Search for recipes">
-    <!-- Add additional form fields for criteria -->
     <input type="text" name="cuisine" placeholder="Cuisine">
     <input type="text" name="diet" placeholder="Diet">
     <!-- Add more fields as needed -->
+    <input type="hidden" name="user_id" value="<?php echo isset($_SESSION['user_id']) ? $_SESSION['user_id'] : ''; ?>">
     <button type="submit">Search</button>
+   
 </form>
 
 <!-- Display the number of results -->
